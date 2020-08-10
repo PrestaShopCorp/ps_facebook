86a6282030c44f18fcf509257e7f15db7dab8412
Poulinho <poulai_a@etna-alternance.net>
2020-08-10 10:34:01 +0200

    add vue js config

---
+++ _dev/stories/Welcome.vue
@@ -0,0 +1,125 @@
+// eslint-disable-next-line no-console
+<template>
+  <div class="main">
+    <h1>Welcome to STORYBOOK</h1>
+    <p>
+      This is a UI component dev environment for your app.
+    </p>
+    <p>
+      We've added some basic stories inside the
+      <br />
+      <code class="code">src/stories</code>
+      <br />
+      directory.
+      <br />
+      A story is a single state of one or more UI components. You can have as many stories as
+      you want.
+      <br />
+      (Basically a story is like a visual test case.)
+    </p>
+    <p>
+      See these sample
+      <br />
+      <a class="link" @click="onClick" role="button" tabIndex="0">stories</a>
+      <br />
+      for a component called
+      <br />
+      <code class="code">Button</code>
+      .
+    </p>
+    <p>
+      Just like that, you can add your own components as stories.
+      <br />
+      You can also edit those components and see changes right away.
+      <br />
+      (Try editing the <code class="code">Button</code> component
+      located at <code class="code">src/stories/Button.js</code>.)
+    </p>
+    <p>
+      This is just one thing you can do with Storybook.
+      <br />
+      Have a look at the
+      <br />
+      <a
+        class="link"
+        href="https://github.com/storybookjs/storybook"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Storybook
+      </a>
+      <br />
+      repo for more information.
+    </p>
+    <p class="note">
+      <b>NOTE:</b>
+      <br />
+      Have a look at the
+      <br />
+      <code class="code">.storybook/webpack.config.js</code>
+      <br />
+      to add webpack
+      loaders and plugins you are using in this project.
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import {
+  defineComponent,
+  reactive,
+  watch,
+  onMounted
+} from "@vue/composition-api";
+
+const log = () => console.log('Welcome to storybook!');
+
+export default defineComponent({
+  name: 'welcome',
+  props: {
+    showApp: {
+      type: Function,
+      default: log,
+    },
+  },
+  setup(props) {
+    const onClick = (event) => {
+      event.preventDefault();
+      props.showApp();
+    },
+    return {
+      onClick
+    }
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.main {
+  padding: 15px;
+  line-height: 1.4;
+  font-family: 'Helvetica Neue, Helvetica, Segoe UI, Arial, freesans, sans-serif';
+  background-color: '#ffffff';
+  .logo {
+    width: 200px;
+  }
+  .link {
+    color: '#1474f3';
+    text-decoration: 'none';
+    border-radius: 1px solid #1474f3;
+    padding-bottom: 2px;
+  }
+  .code {
+    font-size: 15;
+    font-weight: 600;
+    padding: 2px 5px;
+    border: 1px solid #eae9e9;
+    border-radius: 4;
+    background-color: '#f3f2f2';
+    color: '#3a3a3a';
+  }
+  .note {
+    opacity: 0.5;
+  }
+}
+</style>
---
+++ _dev/vue.config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+
+module.exports = {
+  chainWebpack: (config) => {
+    config.plugins.delete("html");
+    config.plugins.delete("preload");
+    config.plugins.delete("prefetch");
+    config.resolve.alias.set("@", path.resolve(__dirname, "src"));
+  },
+  css: {
+    extract: false,
+  },
+  runtimeCompiler: true,
+  productionSourceMap: false,
+  filenameHashing: false,
+  outputDir: "../views/",
+  assetsDir: "",
+  publicPath: "/modules/ps_facebook/views/",
+};
