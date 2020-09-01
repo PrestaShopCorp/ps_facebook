64df544b562ef6b86ef37b8e21af42c6c06aa9bc
zalexki <haumalex@gmail.com>
2020-09-01 15:09:05 +0200

    continue resolver

--- classes/Resolver/EventResolver.php
+++ classes/Resolver/EventResolver.php
@@ -1,25 +1,39 @@
 <?php 
 
+namespace PrestaShop\Module\PrestashopFacebook\Resolver;
+
+use PrestaShop\Module\PrestashopFacebook\Handler\PixelHandler;
+use PrestaShop\Module\PrestashopFacebook\Handler\ApiConversionHandler;
+
 class EventResolver
 {
+    public function __construct() 
+    {
+        $this->conversionHandler = new ApiConversionHandler();
+        $this->pixelHandler = new PixelHandler();
+    }
+
+    /**
+     * resolve if event will be sent via js variable in TPL or is handled elsewhere in JS
+     *
+     * @param string $name
+     * @param array $params
+     *
+     * @return void
+     */
     public function resolve(string $name, array $params)
     {
         switch ($name) {
+            // pixel handled by core js event updateCart
             case 'hookActionCartSave':
                 $this->sendConversionEvent($name, $params);
-                // pixel handled by core js event updateCart
-            break;
-
-            case 'hookActionSearch':
-                $this->sendConversionEvent($name, $params);
-                $this->sendPixelEvent($name, $params);
             break;
             
             default:
-                # code...
-                break;
+                $this->sendConversionEvent($name, $params);
+                $this->sendPixelEvent($name, $params);
+            break;
         }
-        // resolve if event will be sent via js variable in TPL or is handled elsewhere in JS
     }
 
     private function sendConversionEvent($name, $params)
