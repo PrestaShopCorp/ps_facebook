c0235d82087411f70362cac0af075ae712e2b802
marius <marius.gudauskis@invertus.eu>
2020-09-18 16:34:29 +0300

    Checked and added all mocked fields with validations like all lower cases or only numbers

--- classes/Handler/ApiConversionHandler.php
+++ classes/Handler/ApiConversionHandler.php
@@ -2,13 +2,10 @@
 
 namespace PrestaShop\Module\PrestashopFacebook\Handler;
 
-use Context;
 use FacebookAds\Api;
 use FacebookAds\Logger\CurlLogger;
-use PrestaShop\Module\PrestashopFacebook\Adapter\ToolsAdapter;
-use PrestaShop\Module\PrestashopFacebook\Event\Conversion\AddToCartEvent;
-use PrestaShop\Module\PrestashopFacebook\Event\Conversion\SearchEvent;
-use PrestaShop\Module\PrestashopFacebook\Repository\ProductRepository;
+use FacebookAds\Object\ServerSide\Gender;
+use FacebookAds\Object\ServerSide\UserData;
 
 class ApiConversionHandler
 {
@@ -17,37 +14,26 @@ class ApiConversionHandler
      */
     private $facebookBusinessSDK;
 
-    /**
-     * @var Context
-     */
-    private $context;
-
     public function __construct()
     {
         Api::init(
-            null, // app_id
-            null, // app_secret
+            'null', // app_id
+            'null', // app_secret
             \Configuration::get('PS_FBE_ACCESS_TOKEN') // access_token
         );
 
         $this->facebookBusinessSDK = Api::instance();
         $this->facebookBusinessSDK->setLogger(new CurlLogger());
-        $this->context = Context::getContext();
     }
 
-    public function handleEvent($eventName, $params)
+    public function handleEvent($eventName, $event)
     {
-        $pixelId = \Configuration::get('PS_PIXEL_ID');
-
         // TODO: add logic to handle different event
         switch ($eventName) {
             case 'hookActionSearch':
-                (new SearchEvent($this->context, $pixelId))->send($params);
+                // (new SearchEvent($this->context))->send($event);
                 break;
-            case 'hookActionCartSave':
-                (new AddToCartEvent($this->context, $pixelId, new ToolsAdapter(), new ProductRepository()))
-                    ->send($params);
-                break;
+
             case 'hookDisplayHeader':
                 // (new ViewContentEvent($this->context))->send($event);
                 break;
@@ -56,5 +42,29 @@ class ApiConversionHandler
                 // unsupported event
                 break;
         }
+    }
+
+    /**
+     * @return UserData
+     */
+    private function createSdkUserData()
+    {
+        return (new UserData())
+            ->setFbc('fb.1.1554763741205.AbCdEfGhIjKlMnOpQrStUvWxYz1234567890')
+            // It is recommended to send Client IP and User Agent for ServerSide API Events.
+            ->setClientIpAddress($_SERVER['REMOTE_ADDR'])
+            ->setClientUserAgent($_SERVER['HTTP_USER_AGENT'])
+            ->setFbp('fb.1.1558571054389.1098115397')
+            ->setEmail(strtolower('joe@eg.com'))
+            ->setFirstName(strtolower('Joe'))
+            ->setLastName(strtolower('Bean'))
+            ->setPhone(preg_replace('/[^0-9.]+/', '', '+29712345678'))
+            ->setExternalId('username')
+            ->setGender(new Gender(Gender::MALE))
+            ->setDateOfBirth(preg_replace('/[^0-9.]+/', '', '1990-05-10'))
+            ->setCity(strtolower('London'))
+            ->setState(strtolower('ca'))
+            ->setZipCode(preg_replace('/[^0-9.]+/', '', '1234 AA'))
+            ->setCountryCode(strtolower('en'));
     }
 }
