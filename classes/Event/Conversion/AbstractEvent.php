da634c0730ac01285a3b5433f2bf7b3fb056f69c
marius <marius.gudauskis@invertus.eu>
2020-09-23 10:55:19 +0300

    Improved conversion sdk user creation, added abstract class and created search event

---
+++ classes/Event/Conversion/AbstractEvent.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace PrestaShop\Module\PrestashopFacebook\Event\Conversion;
+
+use Configuration;
+use Context;
+use Country;
+use FacebookAds\Object\ServerSide\Gender;
+use FacebookAds\Object\ServerSide\UserData;
+use PrestaShop\Module\PrestashopFacebook\Event\ConversionEventInterface;
+use PrestaShop\PrestaShop\Adapter\Entity\Gender as PsGender;
+
+abstract class AbstractEvent implements ConversionEventInterface
+{
+    /**
+     * @var Context
+     */
+    protected $context;
+
+    /**
+     * @var false|string
+     */
+    protected $pixelId;
+
+    public function __construct(Context $context)
+    {
+        $this->context = $context;
+        $this->pixelId = Configuration::get('PS_PIXEL_ID');
+    }
+
+    /**
+     * @param Context $context
+     *
+     * @return UserData
+     * @throws \PrestaShopDatabaseException
+     * @throws \PrestaShopException
+     */
+    protected function createSdkUserData(Context $context)
+    {
+        $customer = $context->customer;
+        $address = $customer->getAddresses($context->language->id)[0];
+        $psGender = new PsGender($context->customer->id_gender, $context->language->id);
+        $gender = $psGender->type ? Gender::FEMALE : Gender::MALE;
+        $country = new Country($address['id_country']);
+
+        return (new UserData())
+            ->setFbc('fb.1.1554763741205.AbCdEfGhIjKlMnOpQrStUvWxYz1234567890')
+            // It is recommended to send Client IP and User Agent for ServerSide API Events.
+            ->setClientIpAddress($_SERVER['REMOTE_ADDR'])
+            ->setClientUserAgent($_SERVER['HTTP_USER_AGENT'])
+            ->setFbp('fb.1.1558571054389.1098115397')
+            ->setEmail(strtolower($customer->email))
+            ->setFirstName(strtolower($customer->firstname))
+            ->setLastName(strtolower($customer->lastname))
+            ->setPhone(preg_replace('/[^0-9.]+/', '', $address['phone']))
+            ->setGender($gender)
+            ->setDateOfBirth(preg_replace('/[^0-9.]+/', '', $customer->birthday))
+            ->setCity(strtolower($address['city']))
+            ->setState(strtolower($address['state_iso']))
+            ->setZipCode(preg_replace('/[^0-9.]+/', '', $address['postcode']))
+            ->setCountryCode(strtolower($country->iso_code));
+    }
+}
--- classes/Handler/ApiConversionHandler.php
+++ classes/Handler/ApiConversionHandler.php
@@ -2,10 +2,10 @@
 
 namespace PrestaShop\Module\PrestashopFacebook\Handler;
 
+use Context;
 use FacebookAds\Api;
 use FacebookAds\Logger\CurlLogger;
-use FacebookAds\Object\ServerSide\Gender;
-use FacebookAds\Object\ServerSide\UserData;
+use PrestaShop\Module\PrestashopFacebook\Event\Conversion\SearchEvent;
 
 class ApiConversionHandler
 {
@@ -13,6 +13,11 @@ class ApiConversionHandler
      * @var Api|null
      */
     private $facebookBusinessSDK;
+
+    /**
+     * @var Context
+     */
+    private $context;
 
     public function __construct()
     {
@@ -24,6 +29,7 @@ class ApiConversionHandler
 
         $this->facebookBusinessSDK = Api::instance();
         $this->facebookBusinessSDK->setLogger(new CurlLogger());
+        $this->context = Context::getContext();
     }
 
     public function handleEvent($eventName, $event)
@@ -31,7 +37,7 @@ class ApiConversionHandler
         // TODO: add logic to handle different event
         switch ($eventName) {
             case 'hookActionSearch':
-                // (new SearchEvent($this->context))->send($event);
+                 (new SearchEvent($this->context))->send($event);
                 break;
 
             case 'hookDisplayHeader':
@@ -42,29 +48,5 @@ class ApiConversionHandler
                 // unsupported event
                 break;
         }
-    }
-
-    /**
-     * @return UserData
-     */
-    private function createSdkUserData()
-    {
-        return (new UserData())
-            ->setFbc('fb.1.1554763741205.AbCdEfGhIjKlMnOpQrStUvWxYz1234567890')
-            // It is recommended to send Client IP and User Agent for ServerSide API Events.
-            ->setClientIpAddress($_SERVER['REMOTE_ADDR'])
-            ->setClientUserAgent($_SERVER['HTTP_USER_AGENT'])
-            ->setFbp('fb.1.1558571054389.1098115397')
-            ->setEmail(strtolower('joe@eg.com'))
-            ->setFirstName(strtolower('Joe'))
-            ->setLastName(strtolower('Bean'))
-            ->setPhone(preg_replace('/[^0-9.]+/', '', '+29712345678'))
-            ->setExternalId('username')
-            ->setGender(Gender::MALE)
-            ->setDateOfBirth(preg_replace('/[^0-9.]+/', '', '1990-05-10'))
-            ->setCity(strtolower('London'))
-            ->setState(strtolower('ca'))
-            ->setZipCode(preg_replace('/[^0-9.]+/', '', '1234 AA'))
-            ->setCountryCode(strtolower('en'));
     }
 }
