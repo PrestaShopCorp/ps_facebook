59b01f175b90c0578fcfcfee6a6cb6c503cad2b7
marius <marius.gudauskis@invertus.eu>
2020-09-24 10:18:09 +0300

    fixed issue with unkown exception in product repo

--- classes/Repository/ProductRepository.php
+++ classes/Repository/ProductRepository.php
@@ -29,19 +29,14 @@ class ProductRepository
      */
     public function getIdProductAttributeByIdAttributes($idProduct, $idAttributes, $findBest = false)
     {
-        $idProduct = (int)$idProduct;
+        $idProduct = (int) $idProduct;
 
         if (!is_array($idAttributes) && is_numeric($idAttributes)) {
-            $idAttributes = [(int)$idAttributes];
+            $idAttributes = [(int) $idAttributes];
         }
 
         if (!is_array($idAttributes) || empty($idAttributes)) {
-            throw new PrestaShopException(
-                sprintf(
-                    'Invalid parameter $idAttributes with value: "%s"',
-                    print_r($idAttributes, true)
-                )
-            );
+            throw new PrestaShopException(sprintf('Invalid parameter $idAttributes with value: "%s"', print_r($idAttributes, true)));
         }
 
         $idAttributesImploded = implode(',', array_map('intval', $idAttributes));
@@ -92,7 +87,7 @@ class ProductRepository
                         `' . _DB_PREFIX_ . 'product_attribute_combination` pac
                         INNER JOIN `' . _DB_PREFIX_ . 'product_attribute` pa ON pa.id_product_attribute = pac.id_product_attribute
                     WHERE
-                        pa.id_product = ' . (int)$idProduct . '
+                        pa.id_product = ' . (int) $idProduct . '
                         AND pac.id_attribute IN (' . implode(',', array_map('intval', $orderred)) . ')
                     GROUP BY
                         pac.id_product_attribute
@@ -103,9 +98,9 @@ class ProductRepository
         }
 
         if (empty($idProductAttribute)) {
-            throw new PrestaShopObjectNotFoundException('Can not retrieve the id_product_attribute');
+            throw new PrestaShopException('Can not retrieve the id_product_attribute');
         }
 
-        return $idProductAttribute;
+        return (int) $idProductAttribute;
     }
 }
