1de758f33caaca8e982a1abad527ed68476bf2a1
marius <marius.gudauskis@invertus.eu>
2020-09-24 16:24:23 +0300

    created contact form conversion event

--- classes/Event/Conversion/ContactEvent.php
+++ classes/Event/Conversion/ContactEvent.php
@@ -2,6 +2,7 @@
 
 namespace PrestaShop\Module\PrestashopFacebook\Event\Conversion;
 
+use FacebookAds\Object\ServerSide\CustomData;
 use FacebookAds\Object\ServerSide\Event;
 use FacebookAds\Object\ServerSide\EventRequest;
 
