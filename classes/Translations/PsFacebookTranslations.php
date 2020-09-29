5ce3888dffe7fa0adc126d3c09bce27525d64872
Xavier Gouley <xavier.gouley@gmail.com>
2020-09-29 10:11:20 +0200

    merge dev and add i18n mapping

--- classes/Translations/PsFacebookTranslations.php
+++ classes/Translations/PsFacebookTranslations.php
@@ -61,6 +61,60 @@ class PsFacebookTranslations
                     'help' => $this->module->l('Help', 'PsFacebookTranslations'),
                 ],
             ],
+            'configuration' => [
+                'introduction' => [
+                    'getStarted' => $this->module->l('ConfigurationIntroductionGetStarted', 'PsFacebookTranslations'),
+                    'subTitle' => $this->module->l('ConfigurationIntroductionSubTitle', 'PsFacebookTranslations'),
+                    'proPoints' => $this->module->l('ConfigurationIntroductionProPoints', 'PsFacebookTranslations'),
+                    'resume' => $this->module->l('ConfigurationIntroductionResume', 'PsFacebookTranslations'),
+                    'proPoint1Title' => $this->module->l('ConfigurationIntroductionProPoint1Title', 'PsFacebookTranslations'),
+                    'proPoint1Description' => $this->module->l('ConfigurationIntroductionProPoint1Description', 'PsFacebookTranslations'),
+                    'proPoint2Title' => $this->module->l('ConfigurationIntroductionProPoint2Title', 'PsFacebookTranslations'),
+                    'proPoint2Description' => $this->module->l('ConfigurationIntroductionProPoint2Description', 'PsFacebookTranslations'),
+                    'proPoint3Title' => $this->module->l('ConfigurationIntroductionProPoint3Title', 'PsFacebookTranslations'),
+                    'proPoint3Description' => $this->module->l('ConfigurationIntroductionProPoint3Description', 'PsFacebookTranslations'),
+                    'proPoint4Title' => $this->module->l('ConfigurationIntroductionProPoint4Title', 'PsFacebookTranslations'),
+                    'proPoint4Description' => $this->module->l('ConfigurationIntroductionProPoint4Description', 'PsFacebookTranslations'),
+                    'proPoint5Title' => $this->module->l('ConfigurationIntroductionProPoint5Title', 'PsFacebookTranslations'),
+                    'proPoint5Description' => $this->module->l('ConfigurationIntroductionProPoint5Description', 'PsFacebookTranslations'),
+                    'needMoreFeatures' => $this->module->l('ConfigurationIntroductionNeedMoreFeatures', 'PsFacebookTranslations'),
+                    'seeDetailedPlans' => $this->module->l('ConfigurationIntroductionSeeDetailedPlans', 'PsFacebookTranslations'),
+                ],
+                'messages' => [
+                    'success' => $this->module->l('ConfigurationMessagesSuccess', 'PsFacebookTranslations'),
+                    'syncCatalogAdvice' => $this->module->l('ConfigurationMessagesSyncCatalogAdvice', 'PsFacebookTranslations'),
+                    'syncCatalogButton' => $this->module->l('ConfigurationMessagesSyncCatalogButton', 'PsFacebookTranslations'),
+                ],
+                'facebook' => [
+                    'title' => $this->module->l('ConfigurationFacebookTitle', 'PsFacebookTranslations'),
+                    'notConnected' => [
+                        'intro' => $this->module->l('ConfigurationFacebookNotConnectedIntro', 'PsFacebookTranslations'),
+                        'connectButton' => $this->module->l('ConfigurationFacebookNotConnectedConnectButton', 'PsFacebookTranslations'),
+                        'description' => $this->module->l('ConfigurationFacebookNotConnectedDescription', 'PsFacebookTranslations'),
+                        'details' => $this->module->l('ConfigurationFacebookNotConnectedDetails', 'PsFacebookTranslations'),
+                    ],
+                    'connected' => [
+                        'description' => $this->module->l('ConfigurationFacebookConnectedDescription', 'PsFacebookTranslations'),
+                        'editButton' => $this->module->l('ConfigurationFacebookConnectedEditButton', 'PsFacebookTranslations'),
+                        'facebookBusinessManager' => $this->module->l('ConfigurationFacebookConnectedFacebookBusinessManager', 'PsFacebookTranslations'),
+                        'facebookBusinessManagerTooltip' => $this->module->l('ConfigurationFacebookConnectedFacebookBusinessManagerTooltip', 'PsFacebookTranslations'),
+                        'facebookPixel' => $this->module->l('ConfigurationFacebookConnectedFacebookPixel', 'PsFacebookTranslations'),
+                        'facebookPixelTooltip' => $this->module->l('ConfigurationFacebookConnectedFacebookPixelTooltip', 'PsFacebookTranslations'),
+                        'facebookPage' => $this->module->l('ConfigurationFacebookConnectedFacebookPage', 'PsFacebookTranslations'),
+                        'facebookPageTooltip' => $this->module->l('ConfigurationFacebookConnectedFacebookPageTooltip', 'PsFacebookTranslations'),
+                        'facebookAds' => $this->module->l('ConfigurationFacebookConnectedFacebookAds', 'PsFacebookTranslations'),
+                        'facebookAdsTooltip' => $this->module->l('ConfigurationFacebookConnectedFacebookAdsTooltip', 'PsFacebookTranslations'),
+                    ],
+                ],
+                'app' => [
+                    'like' => $this->module->l('ConfigurationAppLike', 'PsFacebookTranslations'),
+                    'likes' => $this->module->l('ConfigurationAppLikes', 'PsFacebookTranslations'),
+                    'createdAt' => $this->module->l('ConfigurationAppCreatedAt', 'PsFacebookTranslations'),
+                    'lastActive' => $this->module->l('ConfigurationAppLastActive', 'PsFacebookTranslations'),
+                    'activated' => $this->module->l('ConfigurationAppActivated', 'PsFacebookTranslations'),
+                    'disabled' => $this->module->l('ConfigurationAppDisabled', 'PsFacebookTranslations'),
+                ]
+            ],
         ];
 
         return $translations;
