16b4fb0c58836d3e43ea4f2431114f38b15f3257
marius <marius.gudauskis@invertus.eu>
2020-10-05 14:28:10 +0300

    cs fixer

--- classes/Event/Conversion/ContactEvent.php
+++ classes/Event/Conversion/ContactEvent.php
@@ -2,7 +2,6 @@
 
 namespace PrestaShop\Module\PrestashopFacebook\Event\Conversion;
 
-use FacebookAds\Object\ServerSide\CustomData;
 use FacebookAds\Object\ServerSide\Event;
 use FacebookAds\Object\ServerSide\EventRequest;
 
