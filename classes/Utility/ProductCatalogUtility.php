7451b1800b47235daf207428e2060b9ab5dba6a3
marius <marius.gudauskis@invertus.eu>
2020-10-06 14:19:17 +0300

    improved productCatalogUtility function makeProductId

--- classes/Utility/ProductCatalogUtility.php
+++ classes/Utility/ProductCatalogUtility.php
@@ -4,8 +4,21 @@ namespace PrestaShop\Module\Ps_facebook\Utility;
 
 class ProductCatalogUtility
 {
+    /**
+     * @param int $productId
+     * @param int|null $productAttributeId
+     * @param string $locale
+     * @return string
+     */
     public static function makeProductId($productId, $productAttributeId, $locale)
     {
-        return "{$productId}-{$productAttributeId}-{$locale}";
+        return implode(
+            '-',
+            [
+                (int)$productId,
+                (int)$productAttributeId,
+                $locale
+            ]
+        );
     }
 }
