5621c135839000106a0f48bd507e8e2c0450c1e5
Thomas Nabord <thomas.nabord@prestashop.com>
2020-10-09 17:29:05 +0000

    Store FBE Ids in database after onboarding

--- controllers/admin/AdminAjaxPsfacebookController.php
+++ controllers/admin/AdminAjaxPsfacebookController.php
@@ -50,22 +50,9 @@ class AdminAjaxPsfacebookController extends ModuleAdminController
     public function ajaxProcessSaveTokenFbeAccount()
     {
         $token = \Tools::getValue('accessToken');
-        $response = Configuration::updateValue('fbe_access_token', $token);
+        $response = Configuration::updateValue('PS_FACEBOOK_access_token', $token);
 
         $this->ajaxDie(json_encode($response));
-    }
-
-    public function ajaxProcessFacebookWebhooks()
-    {
-        // TODO: add some checks
-        Configuration::updateValue('fbe_pixel_id', \Tools::getValue('pixel_id'));
-        Configuration::updateValue('fbe_business_id', \Tools::getValue('business_id'));
-        Configuration::updateValue('fbe_business_manager_id', \Tools::getValue('business_manager_id'));
-        Configuration::updateValue('fbe_access_token', \Tools::getValue('access_token'));
-        Configuration::updateValue('fbe_profiles', \Tools::getValue('profiles'));
-        Configuration::updateValue('fbe_pages', \Tools::getValue('pages'));
-        Configuration::updateValue('fbe_ad_account_id', \Tools::getValue('ad_account_id'));
-        Configuration::updateValue('fbe_catalog_id', \Tools::getValue('catalog_id'));
     }
 
     /**
