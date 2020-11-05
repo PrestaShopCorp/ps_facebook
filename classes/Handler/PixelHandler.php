5a574db4d12867be88acd6eebe0456bf981c17f0
pablopolyte <pablo.polyte@prestashop.com>
2020-11-05 08:29:01 +0100

    csfixer

--- classes/Handler/PixelHandler.php
+++ classes/Handler/PixelHandler.php
@@ -3,12 +3,12 @@
 namespace PrestaShop\Module\PrestashopFacebook\Handler;
 
 use PrestaShop\Module\PrestashopFacebook\Buffer\TemplateBuffer;
-use PrestaShop\Module\PrestashopFacebook\Event\Pixel\SearchEvent;
+use PrestaShop\Module\PrestashopFacebook\Event\Pixel\CompleteRegistrationEvent;
 use PrestaShop\Module\PrestashopFacebook\Event\Pixel\ContactEvent;
-use PrestaShop\Module\PrestashopFacebook\Event\Pixel\ViewContentEvent;
 use PrestaShop\Module\PrestashopFacebook\Event\Pixel\InitiateCheckoutEvent;
 use PrestaShop\Module\PrestashopFacebook\Event\Pixel\OrderConfirmationEvent;
-use PrestaShop\Module\PrestashopFacebook\Event\Pixel\CompleteRegistrationEvent;
+use PrestaShop\Module\PrestashopFacebook\Event\Pixel\SearchEvent;
+use PrestaShop\Module\PrestashopFacebook\Event\Pixel\ViewContentEvent;
 
 class PixelHandler
 {
