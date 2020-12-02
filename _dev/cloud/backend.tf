c00b23fd6644988d8172dc35fd14c40de72a0ac3
Maxence Perrin <mPerrin@pc-32.home>
2020-12-02 16:52:10 +0100

    WIP - storybook

---
+++ _dev/cloud/backend.tf
@@ -0,0 +1,9 @@
+/*
+ Terraform docs re: configuring back end: https://www.terraform.io/docs/backends/types/gcs.html
+ Use a bucket to store state so we can run it everywhere
+*/
+terraform {
+  backend "gcs" {
+    prefix = "terraform/facebook-storybook"
+  }
+}
