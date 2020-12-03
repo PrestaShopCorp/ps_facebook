6b12b6f5537776b864ba35982d5188ee3bd0bd4a
Thomas Nabord <thomas.nabord@prestashop.com>
2020-12-03 10:51:47 +0000

    Rename handleVisibilityChange with onVisibilityChange

--- _dev/src/views/integrate.vue
+++ _dev/src/views/integrate.vue
@@ -168,7 +168,7 @@ export default defineComponent({
       && this.dynamicUnavailableFeaturesLength === 0
     ) {
       this.fetchData();
-      this.registerToVisibilityChangeEvent();
+      this.registerToWindowVisibilityChangeEvent();
     } else {
       this.loading = false;
     }
@@ -177,7 +177,7 @@ export default defineComponent({
     if (document.removeEventListener === 'undefined' || this.hiddenProp === null) {
       return;
     }
-    document.removeEventListener(this.visibilityChangeEvent, this.handleVisibilityChange, false);
+    document.removeEventListener(this.visibilityChangeEvent, this.onWindowVisibilityChange, false);
   },
   computed: {
     dynamicEnabledFeaturesLength() {
@@ -237,14 +237,14 @@ export default defineComponent({
       this.successfullyEnabledFeatures = this.successfullyEnabledFeatures
         .filter((feature) => feature !== acknowledgedFeature);
     },
-    handleVisibilityChange() {
+    onWindowVisibilityChange() {
       // Watch when the page gets the focus, for instance
       // when the merchant comes back from another tab.
       if (document[this.hiddenProp] === false) {
         this.fetchData();
       }
     },
-    registerToVisibilityChangeEvent() {
+    registerToWindowVisibilityChangeEvent() {
       // https://developer.mozilla.org/en-US/docs/Web/API/Page_Visibility_API
       if (typeof document.hidden !== 'undefined') { // Opera 12.10 and Firefox 18 and later support
         this.hiddenProp = 'hidden';
@@ -260,7 +260,7 @@ export default defineComponent({
       if (document.addEventListener === 'undefined' || this.hiddenProp === null) {
         return;
       }
-      document.addEventListener(this.visibilityChangeEvent, this.handleVisibilityChange, false);
+      document.addEventListener(this.visibilityChangeEvent, this.onWindowVisibilityChange, false);
     },
   },
 });
