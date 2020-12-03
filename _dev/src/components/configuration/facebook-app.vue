32374650c56d101cd919a4b901a53c3af8ed9167
Thomas Nabord <thomas.nabord@prestashop.com>
2020-12-03 16:36:16 +0000

    Fix js errors

--- _dev/src/components/configuration/facebook-app.vue
+++ _dev/src/components/configuration/facebook-app.vue
@@ -154,12 +154,12 @@ export default defineComponent({
       default: null,
     },
     createdAt: {
-      type: Number,
+      type: String,
       required: false,
       default: null,
     },
     lastActive: {
-      type: Number,
+      type: String,
       required: false,
       default: null,
     },
