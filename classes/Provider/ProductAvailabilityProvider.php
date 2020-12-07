d3a5cd57200213f09d8a2cf6dc572f82781ad5a7
margud <marius.gudauskis@invertus.eu>
2020-12-07 13:50:09 +0200

    added extra fields for product availability

--- classes/Provider/ProductAvailabilityProvider.php
+++ classes/Provider/ProductAvailabilityProvider.php
@@ -3,24 +3,43 @@
 namespace PrestaShop\Module\PrestashopFacebook\Provider;
 
 use FacebookAds\Object\Values\ProductItemAvailabilityValues;
+use PrestaShop\Module\PrestashopFacebook\Adapter\ConfigurationAdapter;
 use Product;
 
 class ProductAvailabilityProvider implements ProductAvailabilityProviderInterface
 {
     /**
-     * todo:add more availability cases
-     *
+     * @var ConfigurationAdapter
+     */
+    private $configurationAdapter;
+
+    public function __construct(ConfigurationAdapter $configurationAdapter)
+    {
+        $this->configurationAdapter = $configurationAdapter;
+    }
+
+    /**
      * @param int $productId
      *
      * @return string
+     *
+     * @throws \PrestaShopDatabaseException
+     * @throws \PrestaShopException
      */
     public function getProductAvailability($productId)
     {
-        $productQuantity = Product::getQuantity($productId);
-        if ($productQuantity > 0) {
-            return ProductItemAvailabilityValues::IN_STOCK;
-        }
+        $product = new Product($productId);
 
-        return ProductItemAvailabilityValues::OUT_OF_STOCK;
+        switch ($product->out_of_stock) {
+            case 1:
+                return ProductItemAvailabilityValues::AVAILABLE_FOR_ORDER;
+            case 2:
+                $isAvailable = Product::isAvailableWhenOutOfStock($product->out_of_stock);
+
+                return $isAvailable ? ProductItemAvailabilityValues::AVAILABLE_FOR_ORDER : ProductItemAvailabilityValues::OUT_OF_STOCK;
+            case 0:
+            default:
+                return ProductItemAvailabilityValues::DISCONTINUED;
+        }
     }
 }
