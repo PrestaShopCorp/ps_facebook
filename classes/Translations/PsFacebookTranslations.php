9681d96e4f55add26c35455c720b31e18b200fd1
Thomas Nabord <thomas.nabord@prestashop.com>
2020-12-16 17:36:58 +0000

    Update wording

--- classes/Translations/PsFacebookTranslations.php
+++ classes/Translations/PsFacebookTranslations.php
@@ -60,7 +60,7 @@ class PsFacebookTranslations
             'configuration' => [
                 'introduction' => [
                     'getStarted' => $this->module->l('Get started', 'PsFacebookTranslations'),
-                    'subTitle' => $this->module->l('Build your business presence into Facebook community easily and quickly!', 'PsFacebookTranslations'),
+                    'subTitle' => $this->module->l('Easily and quickly build your business presence into Facebook community!', 'PsFacebookTranslations'),
                     'proPoints' => $this->module->l("- No credit card required \n- Easy setup \n- Cancel anytime", 'PsFacebookTranslations'),
                     'resume' => $this->module->l('Make your first steps with PrestaShop Facebook!', 'PsFacebookTranslations'),
                     'proPoint1Title' => $this->module->l('Manage your business', 'PsFacebookTranslations'),
@@ -94,12 +94,12 @@ class PsFacebookTranslations
                         'intro' => $this->module->l('Integrate your shop with Facebook.', 'PsFacebookTranslations'),
                         'connectButton' => $this->module->l('Connect to Facebook', 'PsFacebookTranslations'),
                         'description' => $this->module->l('With PS Facebook enjoy a frictionless, codeless onboarding and management experience for Facebook business products.', 'PsFacebookTranslations'),
-                        'details' => $this->module->l("- Create/Select business \n- Create/Select page \n- Create/Select ad account \n- Create/Select install pixel \n - Create/Select sync product catalog \n - Create Facebook Shop & Instagram Shopping \n- Customize your Facebook Page with call-to-action", 'PsFacebookTranslations'),
+                        'details' => $this->module->l("- Create/Select business \n- Create/Select page \n- Create/Select ad account \n- Create/Select install pixel \n - Create/Select sync product catalog \n - Create Facebook Shop & Instagram Shopping \n- Add Messenger plug-in in your shop", 'PsFacebookTranslations'),
                     ],
                     'connected' => [
                         'description' => $this->module->l('You authorize this Facebook account to connect to your store:', 'PsFacebookTranslations'),
                         'editButton' => $this->module->l('Restart onboarding', 'PsFacebookTranslations'),
-                        'unlinkButton' => $this->module->l('Unlink', 'PsFacebookTranslations'),
+                        'unlinkButton' => $this->module->l('Logout', 'PsFacebookTranslations'),
                         'facebookBusinessManager' => $this->module->l('Facebook Business Manager', 'PsFacebookTranslations'),
                         'facebookBusinessManagerTooltip' => $this->module->l('Facebook business account allows you to manage advertising accounts, Pages and the people who work on them in the same place', 'PsFacebookTranslations'),
                         'facebookPixel' => $this->module->l('Facebook Pixel', 'PsFacebookTranslations'),
@@ -139,7 +139,7 @@ class PsFacebookTranslations
                 'exportCatalogButton' => $this->module->l('Share catalog', 'PsFacebookTranslations'),
                 'catalogExportIntro' => $this->module->l('Before buyers can purchase items from you, you\'ll need to upload your products information into a Facebook Product Catalog.', 'PsFacebookTranslations'),
                 'catalogExportInfo' => $this->module->l("Please be aware of the following: \n1. All « active » products are exported to Facebook for their base language.\n2. Export of product localizations (for translations) are still in beta and could trigger some unexpected errors.\n3. It can take up to 24h for your PrestaShop product information to sync to Facebook.\n4. The entire catalog is updated with a recurrent update every 24 hours.\n5. Each modification made to your catalog will be taken into account during the next synchronization.\n6. Only products with mandatory fields filled will be synchronized: a (short) description, a quantity, a price, a cover, a manufacturer or an ean/upc/isbn.", 'PsFacebookTranslations'),
-                'preApprovalScanTitle' => $this->module->l('Pre-approval scan', 'PsFacebookTranslations'),
+                'preApprovalScanTitle' => $this->module->l('Product scan before synchronization to facebook', 'PsFacebookTranslations'),
                 'preApprovalScanIntro' => $this->module->l('Check which products are ready to be synchronized and which ones have problems and will not be synchronized.', 'PsFacebookTranslations'),
                 'preApprovalScanRefreshDate' => $this->module->l('Last scan today at {0}', 'PsFacebookTranslations'),
                 'preApprovalScanReadyToSync' => $this->module->l('Ready to sync', 'PsFacebookTranslations'),
@@ -148,7 +148,7 @@ class PsFacebookTranslations
                 'catalogExportDisclaimer' => $this->module->l('By sharing your catalog, you agree PrestaShop may send all information related to the catalog products to Facebook.', 'PsFacebookTranslations'),
                 'catalogExportPaused' => $this->module->l('Paused', 'PsFacebookTranslations'),
                 'catalogExportActivated' => $this->module->l('Activated', 'PsFacebookTranslations'),
-                'catalogExportNotice' => $this->module->l("It can take up to 24h for your PrestaShop product information to sync to Facebook. \nOnce products are published you can start tagging them on Instagram or create Facebook ads.", 'PsFacebookTranslations'),
+                'catalogExportNotice' => $this->module->l('It can take up to 24h for your PrestaShop product information to sync to Facebook.', 'PsFacebookTranslations'),
                 'catalogExportOperationPaused' => $this->module->l('The operation is paused.', 'PsFacebookTranslations'),
                 'viewButton' => $this->module->l('View', 'PsFacebookTranslations'),
                 'viewCatalogButton' => $this->module->l('View catalog', 'PsFacebookTranslations'),
@@ -170,7 +170,7 @@ class PsFacebookTranslations
             ],
             'categoryMatching' => [
                 'title' => $this->module->l('Enhance your catalog : category mapping', 'PsFacebookTranslations'),
-                'intro' => $this->module->l('To enhance your catalog and help customers discover your items online, enter a Facebook product category (FPC) for your items and then add more information specific to each category. We recommend that you choose the most specific category possible that applies to each item to help customers understand what type of item you’re selling.', 'PsFacebookTranslations'),
+                'intro' => $this->module->l('To enhance your catalog and help customers discover your items online, enter a Google product category (GPC) for your items.', 'PsFacebookTranslations'),
                 'counterSubTitle' => $this->module->l('specified parent categories', 'PsFacebookTranslations'),
                 'autocomplete' => [
                     'typeToFilter' => $this->module->l('Type to filter', 'PsFacebookTranslations'),
@@ -181,9 +181,9 @@ class PsFacebookTranslations
                 ],
                 'tableMatching' => [
                     'firstTd' => $this->module->l('Category on your site'),
-                    'secondTd' => $this->module->l('Facebook category'),
+                    'secondTd' => $this->module->l('Google category'),
                     'thirdTd' => $this->module->l('Parent category'),
-                    'fourthTd' => $this->module->l('Facebook subcategory'),
+                    'fourthTd' => $this->module->l('Google subcategory'),
                 ],
             ],
             'integrate' => [
