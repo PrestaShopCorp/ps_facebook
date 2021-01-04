761d455ff727a30c5d8c7c4582558d91d3c4b0dd
margud <marius.gudauskis@invertus.eu>
2021-01-04 16:49:16 +0200

    cs fixer

--- tests/unit/Handler/GoogleProductHandlerTest.php
+++ tests/unit/Handler/GoogleProductHandlerTest.php
@@ -132,7 +132,7 @@ class GoogleProductHandlerTest extends TestCase
                         'message' => 'error message',
                     ],
                 ],
-            ]
+            ],
         ];
     }
 }
