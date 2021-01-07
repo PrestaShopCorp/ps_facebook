1d0d2b035a05eb5301e54140b4177d672cca20d6
Xavier Gouley <xavier.gouley@prestashop.com>
2021-01-07 11:32:05 +0100

    Update ps_facebook.php

--- ps_facebook.php
+++ ps_facebook.php
@@ -149,7 +149,7 @@ class Ps_facebook extends Module
     {
         $this->name = 'ps_facebook';
         $this->tab = 'advertising_marketing';
-        $this->version = '1.2.0';
+        $this->version = '1.3.0';
         $this->author = 'PrestaShop';
         $this->need_instance = 0;
         $this->module_key = '860395eb54512ec72d98615805274591';
