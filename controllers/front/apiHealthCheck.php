1a3090c0a102257f79573f1b43515feb8fe6c01e
margud <marius.gudauskis@invertus.eu>
2021-01-12 15:30:47 +0200

    added healtcheck front controller to get information about module

---
+++ controllers/front/apiHealthCheck.php
@@ -0,0 +1,64 @@
+<?php
+
+use PrestaShop\Module\PrestashopFacebook\Repository\ServerInformationRepository;
+use PrestaShop\Module\PsAccounts\Config\Config;
+
+class ps_FacebookApiHealthCheckModuleFrontController extends ModuleFrontController
+{
+    public function init()
+    {
+        $a = 1;
+    }
+
+    /**
+     * @return void
+     */
+    public function postProcess()
+    {
+        /** @var ServerInformationRepository $serverInformationRepository */
+        $serverInformationRepository = $this->module->getService(ServerInformationRepository::class);
+
+        $status = $serverInformationRepository->getHealthCheckData();
+
+        $this->exitWithResponse($status);
+    }
+
+    /**
+     * @param array $response
+     *
+     * @return void
+     */
+    protected function exitWithResponse(array $response)
+    {
+        $httpCode = isset($response['httpCode']) ? (int) $response['httpCode'] : 200;
+
+        $this->dieWithResponse($response, $httpCode);
+    }
+
+    /**
+     * @param array $response
+     * @param int $code
+     *
+     * @return void
+     */
+    private function dieWithResponse(array $response, $code)
+    {
+        $httpStatusText = "HTTP/1.1 $code";
+
+        if (array_key_exists((int) $code, Config::HTTP_STATUS_MESSAGES)) {
+            $httpStatusText .= ' ' . Config::HTTP_STATUS_MESSAGES[(int) $code];
+        } elseif (isset($response['body']['statusText'])) {
+            $httpStatusText .= ' ' . $response['body']['statusText'];
+        }
+
+        $response['httpCode'] = (int) $code;
+
+        header('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
+        header('Content-Type: application/json;charset=utf-8');
+        header($httpStatusText);
+
+        echo json_encode($response, JSON_UNESCAPED_SLASHES);
+
+        exit;
+    }
+}
