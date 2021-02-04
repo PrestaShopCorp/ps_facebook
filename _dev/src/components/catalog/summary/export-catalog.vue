7c76cc9977dd1936285a28b15325357dd82bf4d9
Xavier Gouley <xavier.gouley@gmail.com>
2021-02-04 12:16:08 +0100

    fix false error for reset link, bump module version to 1.4.4

--- _dev/src/components/catalog/summary/export-catalog.vue
+++ _dev/src/components/catalog/summary/export-catalog.vue
@@ -486,11 +486,8 @@ export default defineComponent({
         if (!res.ok) {
           throw new Error(res.statusText || res.status);
         }
-        return res.json();
-      }).then((res) => {
-        if (!res.success) {
-          throw new Error(res.statusText || res.status);
-        }
+        // return res.json();
+      }).then(() => {
         this.resetLinkSuccess = setTimeout(() => {
           this.resetLinkSuccess = null;
         }, 5000);
