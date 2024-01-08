60e09921363c602e603c058b7cc7456cb9fbc7dd
Thomas Nabord <thomas.nabord@prestashop.com>
2024-01-08 15:20:50 +0000

    Fix file name

--- _dev/src/bc-ante-vite.ts
+++ _dev/src/bc-ante-vite.ts
@@ -1,5 +1,5 @@
 const scriptElement = document.createElement('script');
 scriptElement.type = 'text/javascript';
-scriptElement.src = (document.currentScript as HTMLScriptElement).src.replace('app.js', 'psxmarketingwithgoogle-ui.js');
+scriptElement.src = (document.currentScript as HTMLScriptElement).src.replace('app.js', 'pssocial-ui.js');
 scriptElement.type = 'module';
 document.head.append(scriptElement);
