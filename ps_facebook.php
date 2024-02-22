aae08dd1f4314560c767ee1b89dc3dca32f5a7ea
Thomas Nabord <thomas.nabord@prestashop.com>
2024-02-22 16:44:08 +0100

    Bump to v1.36.0

--- ps_facebook.php
+++ ps_facebook.php
@@ -89,7 +89,7 @@ class Ps_facebook extends Module
     {
         $this->name = 'ps_facebook';
         $this->tab = 'advertising_marketing';
-        $this->version = '1.35.0';
+        $this->version = '1.36.0';
         $this->author = 'PrestaShop';
         $this->need_instance = 0;
         $this->module_key = '860395eb54512ec72d98615805274591';
